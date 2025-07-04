9c48ba3974b91e807721808964a466a25ffddb97
Kashish-grewal <kashishgrewal24@gmail.com>
2025-07-05 02:23:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var image1 = document.querySelectorAll("img")[0];
+if(randomNumber1==1){
+image1.setAttribute("src","images/dice1.png" );
+}
+ else if(randomNumber1==2){
+image1.setAttribute("src","images/dice2.png" );
+}
+else if(randomNumber1==3){
+image1.setAttribute("src","images/dice3.png" );
+}
+else if(randomNumber1==4){
+image1.setAttribute("src","images/dice4.png" );
+}
+else if(randomNumber1==5){
+image1.setAttribute("src","images/dice5.png" );
+}
+else if(randomNumber1==6){
+image1.setAttribute("src","images/dice6.png" );
+}
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var image2 = document.querySelectorAll("img")[1];
+if(randomNumber2==1){
+image2.setAttribute("src","images/dice1.png" );
+}
+ else if(randomNumber2==2){
+image2.setAttribute("src","images/dice2.png" );
+}
+else if(randomNumber2==3){
+image2.setAttribute("src","images/dice3.png" );
+}
+else if(randomNumber2==4){
+image2.setAttribute("src","images/dice4.png" );
+}
+else if(randomNumber2==5){
+image2.setAttribute("src","images/dice5.png" );
+}
+else if(randomNumber2==6){
+image2.setAttribute("src","images/dice6.png" );
+}
+
+if(randomNumber1>randomNumber2){
+  document.querySelector(".winner").innerHTML="Player 1 wins";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector(".winner").innerHTML="Player 2 wins";
+}
+else if (randomNumber1==randomNumber2){
+  document.querySelector(".winner").innerHTML="Draw";
+}
